a4f4705a54914a7a674ec85cb6336790d6980a01
Matias Molinas <matias.molinas@gmail.com>
2025-03-11 19:20:24 -0300

    Removing ACP for now. Initially working on a simple system bus implementation.

--- evolving_agents/acp/__init__.py
+++
@@ -1,6 +0,0 @@
-# evolving_agents/acp/__init__.py
-from .client import ACPClient
-from .agent import ACPAgent
-from .tool import ACPTool, ACPToolInterface
-
-__all__ = ['ACPClient', 'ACPAgent', 'ACPTool', 'ACPToolInterface']
